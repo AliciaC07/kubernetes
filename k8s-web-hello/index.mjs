bad2a9a06392e0142a04a1198e3a4baf6c320cd3
root <root@mx-linuz>
2022-08-18 17:02:38 -0400

    Testing sync Argocd

--- k8s-web-hello/index.mjs
+++ k8s-web-hello/index.mjs
@@ -5,7 +5,7 @@ const app = express()
 const PORT = 3000
 
 app.get("/", (req, res)=>{
-    const helloMessage = `VERSION 2: Hello from the ${os.hostname()}`
+    const helloMessage = `VERSION 3: Hello from the ${os.hostname()}`
     console.log(helloMessage)
     res.send(helloMessage)
 
