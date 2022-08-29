7ab3c2a76c05ec956254a4055dacd12009864d84
Alicia <52325101+AliciaC07@users.noreply.github.com>
2022-08-29 14:53:50 -0400

    Testing

--- k8s-web-hello/index.mjs
+++ k8s-web-hello/index.mjs
@@ -3,11 +3,18 @@ import os from 'os'
 
 const app = express()
 const PORT = 3000
+const SECOND_MESSAGE = "empty"
 
 app.get("/", (req, res)=>{
     const helloMessage = `VERSION 3: Hello from the ${os.hostname()}`
     console.log(helloMessage)
     res.send(helloMessage)
+
+})
+
+app.get("/testing", (req, res)=>{
+    console.log(SECOND_MESSAGE)
+    res.send(SECOND_MESSAGE)
 
 })
 
